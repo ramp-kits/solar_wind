15d6fe6a0c7bac8f5c7000b490a61837f269c780
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2018-10-02 09:57:26 +0200

    add download script

---
+++ download_data.py
@@ -0,0 +1,43 @@
+from __future__ import print_function
+
+import os
+
+try:
+    from urllib.request import urlretrieve
+except ImportError:
+    from urllib import urlretrieve
+
+URLBASE = 'https://storage.ramp.studio/solar_wind/{}'
+DATA = [
+    'data_train.parquet', 'data_test.parquet']
+LABELS = [
+    'labels_train.csv', 'labels_test.csv']
+
+
+def main(output_dir='data'):
+    filenames = DATA + LABELS
+    urls = [URLBASE.format(filename) for filename in filenames]
+
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+
+    # notfound = []
+    for url, filename in zip(urls, filenames):
+        output_file = os.path.join(output_dir, filename)
+
+        if os.path.exists(output_file):
+            print("{} already exists".format(output_file))
+            continue
+
+        print("Downloading from {} ...".format(url))
+        urlretrieve(url, filename=output_file)
+        print("=> File saved as {}".format(output_file))
+
+
+if __name__ == '__main__':
+    test = os.getenv('RAMP_TEST_MODE', 0)
+
+    if test:
+        print("Testing mode, not downloading any data.")
+    else:
+        main()
