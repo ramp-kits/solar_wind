ed14bcdcde9798497254d6d934ce3fefc5e8a016
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2018-10-09 09:27:11 +0200

    properly scale for LogisticRegression starting kit submission

--- submissions/starting_kit/classifier.py
+++ submissions/starting_kit/classifier.py
@@ -2,11 +2,13 @@ from __future__ import division
 
 from sklearn.base import BaseEstimator
 from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import make_pipeline
 
 
 class Classifier(BaseEstimator):
     def __init__(self):
-        self.model = LogisticRegression()
+        self.model = make_pipeline(StandardScaler(), LogisticRegression())
 
     def fit(self, X, y):
         self.model.fit(X, y)
