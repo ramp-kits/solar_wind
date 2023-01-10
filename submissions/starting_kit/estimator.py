514b6a8bb8a4a9898c4c50409a53723c9d2a7ce0
Francois Caud <frcaud@gmail.com>
2023-01-10 17:26:18 +0100

    update starting_kit sub

--- submissions/starting_kit/estimator.py
+++ submissions/starting_kit/estimator.py
@@ -3,29 +3,6 @@ from sklearn.pipeline import make_pipeline
 from sklearn.linear_model import LogisticRegression
 
 
-def compute_rolling_std(X_df, feature, time_window, center=False):
-    """
-    For a given dataframe, compute the standard deviation over
-    a defined period of time (time_window) of a defined feature
-
-    Parameters
-    ----------
-    X : dataframe
-    feature : str
-        feature in the dataframe we wish to compute the rolling mean from
-    time_window : str
-        string that defines the length of the time window passed to `rolling`
-    center : bool
-        boolean to indicate if the point of the dataframe considered is
-        center or end of the window
-    """
-    name = '_'.join([feature, time_window, 'std'])
-    X_df[name] = X_df[feature].rolling(time_window, center=center).std()
-    X_df[name] = X_df[name].ffill().bfill()
-    X_df[name] = X_df[name].astype(X_df[feature].dtype)
-    return X_df
-
-
 class FeatureExtractor(BaseEstimator):
 
     def fit(self, X, y):
@@ -57,3 +34,26 @@ def get_estimator():
     pipe = make_pipeline(feature_extractor, classifier)
 
     return pipe
+
+
+def compute_rolling_std(X_df, feature, time_window, center=False):
+    """
+    For a given dataframe, compute the standard deviation over
+    a defined period of time (time_window) of a defined feature
+
+    Parameters
+    ----------
+    X : dataframe
+    feature : str
+        feature in the dataframe we wish to compute the rolling std from
+    time_window : str
+        string that defines the length of the time window passed to `rolling`
+    center : bool
+        boolean to indicate if the point of the dataframe considered is
+        center or end of the window
+    """
+    name = '_'.join([feature, time_window, 'std'])
+    X_df[name] = X_df[feature].rolling(time_window, center=center).std()
+    X_df[name] = X_df[name].ffill().bfill()
+    X_df[name] = X_df[name].astype(X_df[feature].dtype)
+    return X_df
